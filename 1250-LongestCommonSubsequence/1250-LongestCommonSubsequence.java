1cd9920411bccc7775931b6ba03a92a8241181eb
SANDHYA <115525118+sandhyabalamurali@users.noreply.github.com>
2025-06-22 11:03:17 +0530

    Create 1250-LongestCommonSubsequence

---
+++ 1250-LongestCommonSubsequence/1250-LongestCommonSubsequence.java
@@ -0,0 +1,19 @@
+// Last updated: 6/22/2025, 11:03:17 AM
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        int m=text1.length();
+        int n=text2.length();
+        int[][] lcs=new int[m+1][n+1];
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(text1.charAt(i-1)==text2.charAt(j-1)){
+                    lcs[i][j]=1+lcs[i-1][j-1];
+                }
+                else{
+                    lcs[i][j]=Math.max(lcs[i-1][j],lcs[i][j-1]);
+                }
+            }
+        }
+        return lcs[m][n];
+    }
+}
