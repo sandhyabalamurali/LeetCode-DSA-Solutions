b084e1a563b4b25f7231434e6f3f22f34c7f6e2c
SANDHYA <115525118+sandhyabalamurali@users.noreply.github.com>
2025-06-22 11:02:31 +0530

    Create 3685-CountSubarraysOfLengthThreeWithACondition

---
+++ 3685-CountSubarraysOfLengthThreeWithACondition/3685-CountSubarraysOfLengthThreeWithACondition.java
@@ -0,0 +1,13 @@
+// Last updated: 6/22/2025, 11:02:32 AM
+class Solution {
+    public int countSubarrays(int[] nums) {
+        int n = nums.length;
+        int count = 0;
+        for(int i=1;i<n-1;i++){
+            if(nums[i]==(nums[i-1]+nums[i+1])*2){
+                count++;
+            }
+        }
+        return count;
+    }
+}
