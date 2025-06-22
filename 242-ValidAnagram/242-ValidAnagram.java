ee4a581a78b99926aa4910d02e68190bbf1c5f7d
SANDHYA <115525118+sandhyabalamurali@users.noreply.github.com>
2025-06-22 11:04:11 +0530

    Create 242-ValidAnagram

---
+++ 242-ValidAnagram/242-ValidAnagram.java
@@ -0,0 +1,16 @@
+// Last updated: 6/22/2025, 11:04:13 AM
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        for (char c : t.toCharArray()) {
+            if (!map.containsKey(c)) return false;
+            map.put(c, map.get(c) - 1);
+            if (map.get(c) == 0) map.remove(c);
+        }
+        return map.isEmpty();
+    }
+}
