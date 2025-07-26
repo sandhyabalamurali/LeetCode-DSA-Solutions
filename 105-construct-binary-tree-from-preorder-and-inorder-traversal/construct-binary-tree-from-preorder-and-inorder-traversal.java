a3820eaeedf6182301f1cfb85c43919d6c014582
SANDHYA <115525118+sandhyabalamurali@users.noreply.github.com>
2025-07-26 11:46:29 +0530

    Time: 1 ms (95.95%) | Memory: 44.6 MB (40.72%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -14,22 +14,22 @@
  * }
  */
 class Solution {
-    HashMap<Integer,Integer> map=new HashMap<>();
     int index=0;
     public TreeNode buildTree(int[] preorder, int[] inorder) {
+        HashMap<Integer,Integer> map=new HashMap<>();
         for(int i=0;i<inorder.length;i++){
             map.put(inorder[i],i);
         }
-        return helper(preorder,0,inorder.length-1);
+        return helper(preorder,0,inorder.length-1,map);
     }
-    public TreeNode helper(int[] preorder,int left,int right){
+    public TreeNode helper(int[] preorder,int left,int right,HashMap<Integer,Integer> map){
         if(right<left){
             return null;
         }
         int root=preorder[index++];
         TreeNode value=new TreeNode(root);
-        value.left=helper(preorder,left,map.get(root)-1);
-        value.right=helper(preorder,map.get(root)+1,right);
+        value.left=helper(preorder,left,map.get(root)-1,map);
+        value.right=helper(preorder,map.get(root)+1,right,map);
         return value;
     }
 
