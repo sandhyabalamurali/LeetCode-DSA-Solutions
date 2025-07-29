2386f98123f72b9d094791b2065e254f7d926781
SANDHYA <115525118+sandhyabalamurali@users.noreply.github.com>
2025-07-29 08:48:40 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (8.99%) - LeetSync

--- 394-decode-string/decode-string.java
+++ 394-decode-string/decode-string.java
@@ -1,36 +1,31 @@
 class Solution {
-    int i = 0;
-
+    int i=0;
     public String decodeString(String s) {
-
-        StringBuilder sb = new StringBuilder();
-        int count = 0;
-        String temp = "";
-        
-        while (i < s.length()) {
-            char c = s.charAt(i);
-        
+        StringBuilder sb=new StringBuilder();
+        String temp="";
+        int count=0;
+        while(i<s.length()){
+            char c=s.charAt(i);
             i++;
-        
-            if (c == '[') {
-                temp = decodeString(s);
-        
-                int j = 0;
-        
-                while (j < count) {
+            if(c=='['){
+                temp=decodeString(s);
+                int j=0;
+                while(j<count){
                     sb.append(temp);
                     j++;
                 }
-        
-                count = 0;
-            } else if (c == ']')
+                count=0;
+            }
+            else if(c==']'){
                 break;
-            else if (Character.isAlphabetic(c))
+            }
+            else if(Character.isAlphabetic(c)){
                 sb.append(c);
-            else
-                count = count * 10 + c - '0';
+            }
+            else{
+                count=count*10 + c - '0';
+            }
         }
-        
         return sb.toString();
     }
 }
