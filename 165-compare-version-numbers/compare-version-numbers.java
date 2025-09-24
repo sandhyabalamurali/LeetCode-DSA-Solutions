f12fce3a3067589c92edbdb7afe0108d71c50de9
SANDHYA <115525118+sandhyabalamurali@users.noreply.github.com>
2025-09-24 17:40:16 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (97.67%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.java
+++ 165-compare-version-numbers/compare-version-numbers.java
@@ -1,18 +1,23 @@
 class Solution {
     public int compareVersion(String version1, String version2) {
-    String[] levels1 = version1.split("\\.");
-    String[] levels2 = version2.split("\\.");
-    
-    int length = Math.max(levels1.length, levels2.length);
-    for (int i=0; i<length; i++) {
-    	Integer v1 = i < levels1.length ? Integer.parseInt(levels1[i]) : 0;
-    	Integer v2 = i < levels2.length ? Integer.parseInt(levels2[i]) : 0;
-    	int compare = v1.compareTo(v2);
-    	if (compare != 0) {
-    		return compare;
-    	}
+        int i = 0, j = 0;
+        while (i < version1.length() || j < version2.length()) {
+            int num1 = 0, num2 = 0;
+            while (i < version1.length() && version1.charAt(i) != '.') {
+                num1 = num1 * 10 + (version1.charAt(i++) - '0');
+            }
+            while (j < version2.length() && version2.charAt(j) != '.') {
+                num2 = num2 * 10 + (version2.charAt(j++) - '0');
+            }
+            if (num1 < num2) {
+                return -1;
+            }
+            if (num1 > num2) {
+                return 1;
+            }
+            i++;
+            j++;
+        }
+        return 0;
     }
-    
-    return 0;
-}
 }
