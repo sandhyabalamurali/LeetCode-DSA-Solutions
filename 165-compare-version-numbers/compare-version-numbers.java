a63bdf02f1acae0aecf7adc0f59c927a1f7ba10c
SANDHYA <115525118+sandhyabalamurali@users.noreply.github.com>
2025-09-24 17:49:31 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (11.26%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.java
+++ 165-compare-version-numbers/compare-version-numbers.java
@@ -4,10 +4,12 @@ class Solution {
         while (i < version1.length() || j < version2.length()) {
             int num1 = 0, num2 = 0;
             while (i < version1.length() && version1.charAt(i) != '.') {
-                num1 = num1 * 10 + (version1.charAt(i++) - '0');
+                num1 = num1 * 10 + (version1.charAt(i) - '0');
+                i++;
             }
             while (j < version2.length() && version2.charAt(j) != '.') {
-                num2 = num2 * 10 + (version2.charAt(j++) - '0');
+                num2 = num2 * 10 + (version2.charAt(j) - '0');
+                j++;
             }
             if (num1 < num2) {
                 return -1;
